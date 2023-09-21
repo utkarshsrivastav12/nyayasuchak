1e83dbbad6b6c95d4cad2f48ca0b2d9708dcfbef
utkarshsrivastav12 <theutkarsh5@gmail.com>
2023-09-21 19:28:19 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: eNno
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/enno-free-simple-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
